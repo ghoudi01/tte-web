4522f41c90edc2db9c9900d82d22f87f5eba39ec
ghoudi01 <ghoudi31@gmail.com>
2026-04-20 19:50:07 +0100

    f

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,6 @@
-export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
+export const COOKIE_NAME = "tte_auth";
+export const ONE_YEAR_MS = 365 * 24 * 60 * 60 * 1000;
+export const UNAUTHED_ERR_MSG = "Unauthorized";
 
 // Generate login URL at runtime so redirect URI reflects the current origin.
 export const getLoginUrl = () => {
